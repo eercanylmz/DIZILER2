bb393e04df377b63ec8e6187feab8fa06e5c58ca
yilma <yilma@ERCANMONSTER>
2022-12-27 04:35:22 +0300

    Proje dosyası ekle.

---
+++ DIZILER2/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DIZILER2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] series = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            label1.Text = series[2].ToString();
+        }
+    }
+}
